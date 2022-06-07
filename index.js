d4647570b0a3640794d0455037f6eb3da3626974
Brittany Ouellette <brittanyouellette@Brittanys-Air.cabledahmer.local>
2022-06-07 09:46:08 -0500

    update

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
+
+
+
+
 const express = require('express')
 const app = express()
 

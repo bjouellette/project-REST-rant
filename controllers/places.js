ce2ec10e1006c4cf6e7ade4aba2bdb341535941b
bjouellette <brittanyouellette@Brittanys-Air.attlocal.net>
2022-06-11 22:21:06 -0500

    update

--- controllers/places.js
+++ controllers/places.js
@@ -18,4 +18,26 @@ router. get('/', (req,res) => {
       
     res.render('places/index',{places})
 })
+
+router.post('/', (req, res) => {
+  //console.log(req.body)
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
+
+// NEW
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
 module.exports = router

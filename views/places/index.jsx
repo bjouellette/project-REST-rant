204c436c62b054acdbddff2cb7a0ae55e8b34c39
Brittany Ouellette <bjouellette1@gmail.com>
2022-06-11 19:44:14 -0500

    update

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -4,20 +4,29 @@ const Def = require('../default')
 function index (data) {
     let placesFormatted = data.places.map((place) => {
         return (
-            <div>
-                <h2>{place.name}</h2>
-                <img src={place.pic} alt = {place.name}></img>
-            </div>
+          <div className="col-sm-6">
+            <h2>{place.name}</h2>
+            <p clasName="text-center">
+              {place.cuisines}
+            </p>
+            <img src={place.pic} alt={place.name} />
+            <p clasName="text-center">
+              Located in {place.city}, {place.state}
+            </p>
+          </div>
         )
-    })
+      })
+      
     return (
-        <Def>
-            <main>
-                <h1>Places to Rant/Rave About</h1>
+      <Def>
+          <main>
+              <h1>Places to Rant or Rave About</h1>
+              <div className="row">
                 {placesFormatted}
-            </main>
-        </Def>
+              </div>
+          </main>
+      </Def>
     )
-}
-
+  }
+  
 module.exports = index

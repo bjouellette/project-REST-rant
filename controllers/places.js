29629051e22fd3dd8543d19abd6eb8aa6c479614
bjouellette <brittanyouellette@Brittanys-MacBook-Air.local>
2022-06-26 13:34:15 -0500

    update

--- controllers/places.js
+++ controllers/places.js
@@ -3,26 +3,28 @@ const db = require('../models')
 
 router.get('/', (req, res) => {
   db.Place.find()
-    .then((places) => {
-      res.render('places/index', {
-        places
-      })
-    })
-    .catch(err => {
-      console.log(err)
-      res.render('error404')
-    })
+  .then((places) => {
+    res.render('places/index', { places })
+  })
+  .catch(err => {
+    console.log(err)
+    res.render('error404')
+  })
 })
 
 router.post('/', (req, res) => {
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
   db.Place.create(req.body)
-    .then(() => {
+  .then(() => {
       res.redirect('/places')
-    })
-    .catch(err => {
-      console.log('err', err)
-      res.render('error404')
-    })
+  })
+  .catch(err => {
+    console.log('err', err)
+    res.render('error404')
+  })
 })
 
 router.get('/new', (req, res) => {
@@ -31,15 +33,15 @@ router.get('/new', (req, res) => {
 
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
-    .then(place => {
-      res.render('places/show', {
-        place
-      })
-    })
-    .catch(err => {
-      console.log('err', err)
-      res.render('error404')
-    })
+  .populate('comments')
+  .then(place => {
+    console.log(place.comments)
+    res.render('places/show', { place })
+  })
+  .catch(err => {
+    console.log('err', err)
+    res.render('error404')
+  })
 })
 
 router.put('/:id', (req, res) => {
@@ -54,12 +56,29 @@ router.get('/:id/edit', (req, res) => {
   res.send('GET edit form stub')
 })
 
-router.post('/:id/rant', (req, res) => {
-  res.send('GET /places/:id/rant stub')
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
 })
 
 router.delete('/:id/rant/:rantId', (req, res) => {
-  res.send('GET /places/:id/rant/:rantId stub')
+    res.send('GET /places/:id/rant/:rantId stub')
 })
 
 module.exports = router
